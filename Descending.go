2ead5ab313f9c3d8fb114b80db0b0754f59a90f6
karthik <adepkarthik@gmail.com>
2025-12-23 22:21:00 -0500

    Go lang codes

---
+++ Descending.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sl := []int{6, 3, 8, 2, 7, 1}
+
+	for i := 0; i < len(sl); i++ {
+		for j := i + 1; j < len(sl); j++ {
+
+			if sl[i] < sl[j] {
+				sl[i], sl[j] = swap(sl[i], sl[j])
+			}
+		}
+	}
+
+	fmt.Println(sl)
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
+}
---
+++ ran.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var min, max int
+	fmt.Println("Enter the minimum number:")
+	fmt.Scan(&min)
+	fmt.Println("Enter the maximum number:")
+	fmt.Scan(&max)
+
+	var guess int
+	var input string
+
+	for {
+		
+		guess = rand.Intn(max-min+1) + min
+		fmt.Println("Is your number", guess, "?")
+		fmt.Println("Type 'h' if your number is higher, 'l' if lower, 'c' if correct:")
+		fmt.Scan(&input)
+
+		if input == "h" {
+			min = guess + 1
+		} else if input == "l" {
+			max = guess - 1
+		} else if input == "c" {
+			fmt.Println("Yay! I guessed your number!")
+			break
+		} else {
+			fmt.Println("Please type 'h', 'l', or 'c'")
+		}
+	}
+}
